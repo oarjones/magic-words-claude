6385b8e23a0f19d79786df02e0c1b59a5002d7ec
OSCAR ARJONES SANCHEZ <oarjones@gmail.com>
2025-01-19 04:36:59 +0100

    feat: Add visual components and effects- Add TileView for tile visual representation and animations- Add WordEffect for displaying formed words and scores- Add ScoreEffect for score accumulation animations- Implement particle systems for visual feedback- Add smooth animations using DOTween

---
+++ Assets/Scripts/Core/Views/TileView.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+namespace MagicWords.Core.Views
+{
+    public class TileView : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private SpriteRenderer backgroundRenderer;
+        [SerializeField] private TextMeshPro letterText;
+        [SerializeField] private ParticleSystem selectionParticles;
+        [SerializeField] private ParticleSystem freezeParticles;
+        [SerializeField] private ParticleSystem highlightParticles;
+
+        [Header("Visual Settings")]
+        [SerializeField] private Color normalColor = Color.white;
+        [SerializeField] private Color selectedColor;
+        [SerializeField] private Color frozenColor;
+        [SerializeField] private Color objectiveColor;
+
+        private Vector3 originalScale;
+        private Sequence currentAnimation;
+
+        private void Awake()
+        {
+            originalScale = transform.localScale;
+            StopAllParticles();
+        }
+
+        public void SetLetter(char letter)
+        {
+            if (letterText != null)
+            {
+                letterText.text = letter.ToString();
+            }
+        }
+
+        public void PlaySelectAnimation(float duration, float scaleMultiplier)
+        {
+            KillCurrentAnimation();
+
+            currentAnimation = DOTween.Sequence()
+                .Append(transform.DOScale(originalScale * scaleMultiplier, duration))
+                .Join(backgroundRenderer.DOColor(selectedColor, duration))
+                .SetEase(Ease.OutBack);
+
+            if (selectionParticles != null)
+            {
+                selectionParticles.Play();
+            }
+        }
+
+        public void PlayDeselectAnimation(float duration)
+        {
+            KillCurrentAnimation();
+
+            currentAnimation = DOTween.Sequence()
+                .Append(transform.DOScale(originalScale, duration))
+                .Join(backgroundRenderer.DOColor(normalColor, duration))
+                .SetEase(Ease.OutQuad);
+
+            if (selectionParticles != null)
+            {
+                selectionParticles.Stop();
+            }
+        }
+
+        public void PlayInvalidAnimation(float duration, float intensity)
+        {
+            KillCurrentAnimation();
+
+            currentAnimation = DOTween.Sequence()
+                .Append(transform.DOShakePosition(duration, intensity))
+                .Join(backgroundRenderer.DOColor(Color.red, duration * 0.5f))
+                .Append(backgroundRenderer.DOColor(normalColor, duration * 0.5f));
+        }
+
+        public void SetState(TileState state, float transitionDuration = 0.3f)
+        {
+            Color targetColor = state switch
+            {
+                TileState.Normal => normalColor,
+                TileState.Selected => selectedColor,
+                TileState.Frozen => frozenColor,
+                TileState.Objective => objectiveColor,
+                _ => normalColor
+            };
+
+            backgroundRenderer.DOColor(targetColor, transitionDuration);
+
+            UpdateParticles(state);
+        }
+
+        private void UpdateParticles(TileState state)
+        {
+            switch (state)
+            {
+                case TileState.Frozen:
+                    if (freezeParticles != null) freezeParticles.Play();
+                    break;
+                case TileState.Objective:
+                    if (highlightParticles != null) highlightParticles.Play();
+                    break;
+                default:
+                    StopAllParticles();
+                    break;
+            }
+        }
+
+        private void StopAllParticles()
+        {
+            if (selectionParticles != null) selectionParticles.Stop();
+            if (freezeParticles != null) freezeParticles.Stop();
+            if (highlightParticles != null) highlightParticles.Stop();
+        }
+
+        private void KillCurrentAnimation()
+        {
+            currentAnimation?.Kill();
+            currentAnimation = null;
+        }
+
+        private void OnDestroy()
+        {
+            KillCurrentAnimation();
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Views/WordEffect.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using System.Collections;
+
+namespace MagicWords.Core.Views
+{
+    public class WordEffect : MonoBehaviour
+    {
+        [Header("Components")]
+        [SerializeField] private TextMeshProUGUI wordText;
+        [SerializeField] private ParticleSystem validWordParticles;
+        [SerializeField] private ParticleSystem scoreParticles;
+
+        [Header("Settings")]
+        [SerializeField] private float displayDuration = 2f;
+        [SerializeField] private float fadeInDuration = 0.3f;
+        [SerializeField] private float fadeOutDuration = 0.5f;
+        [SerializeField] private float scaleMultiplier = 1.5f;
+        [SerializeField] private float floatDistance = 1f;
+
+        private Vector3 originalScale;
+        private Vector3 originalPosition;
+        private Sequence currentAnimation;
+
+        private void Awake()
+        {
+            originalScale = transform.localScale;
+            originalPosition = transform.position;
+            gameObject.SetActive(false);
+        }
+
+        public void ShowValidWord(string word, int score, Vector3 position)
+        {
+            gameObject.SetActive(true);
+            transform.position = position;
+
+            wordText.text = $"{word}\n+{score}";
+            wordText.alpha = 0f;
+            transform.localScale = originalScale;
+
+            KillCurrentAnimation();
+
+            currentAnimation = DOTween.Sequence()
+                // Fade and scale in
+                .Append(wordText.DOFade(1f, fadeInDuration))
+                .Join(transform.DOScale(originalScale * scaleMultiplier, fadeInDuration))
+                .SetEase(Ease.OutBack)
+
+                // Float up
+                .Join(transform.DOMove(position + Vector3.up * floatDistance, displayDuration)
+                    .SetEase(Ease.OutQuad))
+
+                // Fade out
+                .Append(wordText.DOFade(0f, fadeOutDuration))
+                .Join(transform.DOScale(originalScale, fadeOutDuration))
+                .SetEase(Ease.InQuad)
+                .OnComplete(() => gameObject.SetActive(false));
+
+            if (validWordParticles != null)
+            {
+                validWordParticles.Play();
+            }
+
+            if (score > 0 && scoreParticles != null)
+            {
+                scoreParticles.Play();
+            }
+        }
+
+        public void ShowInvalidWord(string word, Vector3 position)
+        {
+            gameObject.SetActive(true);
+            transform.position = position;
+
+            wordText.text = word;
+            wordText.alpha = 0f;
+            transform.localScale = originalScale;
+
+            KillCurrentAnimation();
+
+            currentAnimation = DOTween.Sequence()
+                .Append(wordText.DOFade(1f, fadeInDuration))
+                .Join(transform.DOScale(originalScale * 0.8f, fadeInDuration))
+                .Join(transform.DOShakePosition(0.5f, 0.1f))
+                .AppendInterval(displayDuration * 0.5f)
+                .Append(wordText.DOFade(0f, fadeOutDuration))
+                .OnComplete(() => gameObject.SetActive(false));
+        }
+
+        private void KillCurrentAnimation()
+        {
+            currentAnimation?.Kill();
+            currentAnimation = null;
+        }
+
+        private void OnDestroy()
+        {
+            KillCurrentAnimation();
+        }
+    }
+}
